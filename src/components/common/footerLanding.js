99f9347479973b6f74cf3d2374945f84238fa3b9
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2021-04-22 22:34:32 -0400

    test

--- src/components/common/footerLanding.js
+++ src/components/common/footerLanding.js
@@ -4,8 +4,6 @@ import csLogo from '../../assets/imgs/CSpireFtLogo.png';
 
 import { BackTop, Image } from 'antd';
 
-// testing
-
 const CityFooter = () => {
   const [date, setDate] = useState();
 
